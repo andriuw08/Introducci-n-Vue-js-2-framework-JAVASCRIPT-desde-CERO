40e634b7d59c895c1311cbdbde17af04188775f5
andriuw08 <andriuw.yepez@gmail.com>
2023-10-31 16:36:55 +0100

    directiva v-html y componentes hijos

--- components/VueDirectives.js
+++ components/VueDirectives.js
@@ -3,11 +3,14 @@
 // v-bind permite hacer reactivos los valores o atributos normales de los elementos js, tambien se puede solo escribir :
 Vue.component('vue-directives', {
     template: ` <div>
+                    <h1 v-text="title"></h1>
                     <p v-text="text"></p>
                     <a v-bind:href="link.href" v-text="link.text"></a>
+                    <directive-html></directive-html>
                 </div>`,
     data () {
         return {
+            title: 'Directivas de Vue.js',
             text: 'Texto de prueba v-text',
             link: {
                 text: 'VueJS Home',
@@ -15,5 +18,8 @@ Vue.component('vue-directives', {
                 title: 'Documentacion Vue JS'
             }
         }
+    },
+    components: {
+        'directive-html': DirectiveHtml
     }
 })
