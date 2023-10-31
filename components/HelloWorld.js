85e6fbc99343b7bf29f9e4f5d3852fb9d4910721
andriuw08 <andriuw.yepez@gmail.com>
2023-10-31 15:53:10 +0100

    Primer Componente con Vuejs

---
+++ components/HelloWorld.js
@@ -0,0 +1,13 @@
+// Crear Hellow World con componentes
+Vue.component('hello-world', {
+    template: ` <header>
+                    <h1>{{ title }}</h1>
+                    <h3>{{ message }}</h3>
+                </header>`,
+    data () {
+        return {
+            title: 'Hola Mundo',
+            message: 'Bienvenido al curso de Vuejs'
+        }
+    }
+})
